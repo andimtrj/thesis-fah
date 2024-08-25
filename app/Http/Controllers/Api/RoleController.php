b33e8cf9af32b44501d139bd3ab7c0e17f786944
fernaldy <fernaldy@ad-ins.com>
2024-08-25 22:34:42 +0700

    Adjust UserController, User Model, Branch Model, Tenant Model, Initial Create Constants.php, add route for registerTenantOwner, registerBranchAdmin, add route with api authorization registeBranchAdmin, createBranch

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -8,12 +8,5 @@ use App\Models\Role;
 
 class RoleController extends Controller
 {
-    public function GetRoleId($role_code){
-        $role = Role::where('role_code', $role_code)->first();
 
-        if($role){
-            return $role->id;
-        }
-        return null;
-    }
 }

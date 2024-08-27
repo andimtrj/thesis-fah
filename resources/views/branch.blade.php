a3576389f040b47793b1de1df015c720bf6317ab
Andi Mataraja <andimtrj@Andis-MacBook-Air.local>
2024-08-27 23:10:01 +0700

    Update Branch Page almost done

--- resources/views/branch.blade.php
+++ resources/views/branch.blade.php
@@ -1,5 +1,155 @@
 <x-master>
   <x-sidebar.sidebar>
-    <h1 class="text-5xl">Branch Page</h1>
+    <h1 class="text-3xl font-medium mb-5">Roji Ramen</h1>
+
+    <div class="flex mb-5 justify-between">
+      <div id="date-range-picker" date-rangepicker class="flex items-center">
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+            </svg>
+          </div>
+          <input id="datepicker-range-start" name="start" type="text"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Select date start">
+        </div>
+        <span class="mx-4 text-gray-500">to</span>
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+            </svg>
+          </div>
+          <input id="datepicker-range-end" name="end" type="text"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Select date end">
+        </div>
+      </div>
+
+      <div class="flex gap-5 items-center">
+
+        <form class="max-w-md mx-auto">
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              </svg>
+            </div>
+            <input type="search" id="default-search"
+              class="block w-full p-2.5 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search" required />
+          </div>
+        </form>
+
+        <button class="flex items-center text-white bg-primary px-3 py-2 rounded-lg gap-1 flex-shrink-0">
+          <svg class="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+            height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M5 12h14m-7 7V5" />
+          </svg>
+          <span>Add Branch</span>
+        </button>
+
+      </div>
+    </div>
+
+    <div class="relative overflow-x-auto rounded-lg">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3 w-12">
+              #
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Location
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Total Branch Admin
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Action
+            </th>
+            <th scope="col" class="px-6 py-3">
+
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              1
+            </th>
+            <td class="px-6 py-4">
+              Silver
+            </td>
+            <td class="px-6 py-4">
+              Laptop
+            </td>
+            <td class="px-6 py-4 flex gap-4 items-center">
+              <a href="" class="bg-green-500 border-2 w-fit p-1 rounded-lg">
+                <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M5 12h14m-7 7V5" />
+                </svg>
+              </a>
+              <a href="" class="bg-accent border-2 w-fit p-1 rounded-lg">
+                <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                </svg>
+              </a>
+              <a href="" class="bg-red-500 border-2 w-fit p-1 rounded-lg">
+                <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                </svg>
+              </a>
+            </td>
+            <td class="px-6 py-4">
+              <a href="" class="bg-primary px-5 py-2 text-white rounded-lg">View Details</a>
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              2
+            </th>
+            <td class="px-6 py-4">
+              White
+            </td>
+            <td class="px-6 py-4">
+              Laptop PC
+            </td>
+            <td class="px-6 py-4">
+              $1999
+            </td>
+          </tr>
+          <tr class="bg-white dark:bg-gray-800">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              3
+            </th>
+            <td class="px-6 py-4">
+              Black
+            </td>
+            <td class="px-6 py-4">
+              Accessories
+            </td>
+            <td class="px-6 py-4">
+              $99
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </x-sidebar.sidebar>
 </x-master>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ Route::get('/', function () {
     return view('register');
 });
 
-<<<<<<< HEAD
 Route::get('/login', function () {
     return view('login');
 });
@@ -19,9 +18,3 @@ Route::get('/login', function () {
 Route::get('/branch', function () {
     return view('branch');
 });
-=======
-
-#region Authenticate
-Route::post('/login', [AuthController::class, 'Authenticate']);
-#endregion
->>>>>>> b33e8cf9af32b44501d139bd3ab7c0e17f786944

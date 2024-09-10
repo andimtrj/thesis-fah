64f00373662f29bbd3afe96d17a0cbe94f513937
fernaldy <fernaldy@ad-ins.com>
2024-09-10 11:29:13 +0700

    Add Logout Function in AuthController and Integrate it with FE

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -31,5 +32,16 @@ class AuthController extends Controller
         }
 
         return response()->json(['message' => 'Login Failed'], 500);
+    }
+
+    public function Logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,33 @@ Route::middleware('web')->group(function () {
 
     Route::get('/login', function () {
         return view('login');
-    });
+    })->name('login');
 
     Route::middleware('auth')->group(function(){
         Route::get('/branch', function () {
             return view('branch');
         })->name('branch');
+
+        Route::get('/menu', function () {
+            return view('menu');
+        })->name('menu');
+
     });
 
-    Route::get('/menu', function () {
-        return view('menu');
-    })->name('menu');
 
 
     Route::get('/landing', function () {
         return view('landing');
-    });
+    })->name('landing');
 
 });
 
 
 
 #region Authenticate
-Route::post('/auth', [AuthController::class, 'Authenticate']);
-Route::post('/registration', [ApiWebController::class, 'Registration']);
+Route::post('/auth', [AuthController::class, 'Authenticate'])->name('auth');
+Route::post('/registration', [ApiWebController::class, 'Registration'])->name('registration');
+Route::post('/logout', [AuthController::class, 'Logout'])->name('logout');
 #endregion
 
 

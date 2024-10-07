a4f93b03b414216f147fcf47c01056672d0c0e3b
Andi Mataraja <amataraja@gmail.com>
2024-10-07 17:42:46 +0700

    Adding selectbox for selecting branch in ingredients and products page

--- resources/views/ingredient.blade.php
+++ resources/views/ingredient.blade.php
@@ -11,7 +11,8 @@
           <h1 class="text-3xl font-medium text-white">{{ $tenant->tenant_name }}</h1>
         </div>
         <div class="flex gap-3">
-          <a href="{{ route('add-ingredient') }}" class="flex items-center text-white bg-accent lg:px-3 md:px-1 py-2 rounded-lg gap-1 flex-shrink-0 shadow-container w-fit md:text-xs lg:text-base">
+          <a href="{{ route('add-ingredient') }}"
+            class="flex items-center text-white bg-accent lg:px-3 md:px-1 py-2 rounded-lg gap-1 flex-shrink-0 shadow-container w-fit md:text-xs lg:text-base">
             <svg class="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -24,6 +25,17 @@
 
       <div class="flex items-end gap-5 px-10 bg-white pt-5 rounded-b-xl">
         <form action="" method="GET" class="flex gap-5 mb-5">
+          <div class="w-[15vw]">
+            <label for="small" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Select a branch</label>
+            <select id="small" required
+              class="block w-full p-2 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary">
+              <option selected>Choose a country</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
           <div class="w-[15vw]">
             <label for="branchCode" class="block mb-1 text-sm font-medium text-gray-900">Field Search 1</label>
             <input type="text" id="branchCode" name="branchCode"
@@ -46,8 +58,7 @@
               </svg>
               <span>Search</span>
             </button>
-            <a href=""
-              class="bg-danger bg-opacity-10 rounded-lg px-5 py-2 text-danger flex items-center gap-1">
+            <a href="" class="bg-danger bg-opacity-10 rounded-lg px-5 py-2 text-danger flex items-center gap-1">
               <span>Clear Search</span>
             </a>
           </div>
@@ -56,7 +67,7 @@
     </div>
 
     <div class="mt-4">
-      <x-ingredient.table/>
+      <x-ingredient.table />
     </div>
   </x-sidebar.sidebar>
 </x-master>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -10,7 +10,8 @@
           <h1 class="text-3xl font-medium text-white">{{ $tenant->tenant_name }}</h1>
         </div>
         <div class="flex gap-3">
-          <a href="" class="flex items-center text-white bg-accent lg:px-3 md:px-1 py-2 rounded-lg gap-1 flex-shrink-0 shadow-container w-fit md:text-xs lg:text-base">
+          <a href=""
+            class="flex items-center text-white bg-accent lg:px-3 md:px-1 py-2 rounded-lg gap-1 flex-shrink-0 shadow-container w-fit md:text-xs lg:text-base">
             <svg class="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
               height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -23,6 +24,17 @@
       {{-- Search Filter --}}
       <div class="flex items-end gap-5 px-10 bg-white pt-5">
         <form action="" method="GET" class="flex gap-5 mb-5">
+          <div class="w-[15vw]">
+            <label for="small" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Select a branch</label>
+            <select id="small" required
+              class="block w-full p-2 text-xs text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary">
+              <option selected>Choose a country</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
           <div class="w-[15vw]">
             <label for="branchCode" class="block mb-1 text-sm font-medium text-gray-900">Product code</label>
             <input type="text" id="branchCode" name="branchCode"
@@ -45,8 +57,7 @@
               </svg>
               <span>Search</span>
             </button>
-            <a href=""
-              class="bg-danger bg-opacity-10 rounded-lg px-5 py-2 text-danger flex items-center gap-1">
+            <a href="" class="bg-danger bg-opacity-10 rounded-lg px-5 py-2 text-danger flex items-center gap-1">
               <span>Clear Search</span>
             </a>
           </div>

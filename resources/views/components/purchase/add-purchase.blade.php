b804833a2dd8f47c64600061f4a09abd44ec5d1d
Andi Mataraja <amataraja@gmail.com>
2024-11-04 21:54:37 +0700

    Update commented code on Purchase Page

--- resources/views/components/purchase/add-purchase.blade.php
+++ resources/views/components/purchase/add-purchase.blade.php
@@ -50,49 +50,49 @@
               <tbody id="table-body-addrow">
                 {{-- <tr class="bg-white border-y text-base text-abu">
                     <td class="px-2 py-3">
-                      <select id="countries"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-primary focus:border-primaring-primary block w-full p-2">
-                        <option selected>Select a product</option>
-                      </select>
-                    </td>
-                    <td class="px-2 w-full">
-                      <div class="flex items-center justify-center">
-                        <button onclick="decreaseValue()"
-                          class="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                          type="button">
-                          <span class="sr-only">Quantity button</span>
-                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 18 2">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M1 1h16" />
-                          </svg>
-                        </button>
-                        <div>
-                          <input type="number" id="first_product" name="ingredients[0][amount]"
-                            class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            value="0" required />
-                        </div>
-                        <button onclick="increaseValue()"
-                          class="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                          type="button">
-                          <span class="sr-only">Quantity button</span>
-                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M9 1v16M1 9h16" />
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                    <td class="px-2">
-                      <input type="text" id="small-input" name="ingredients[0][metrics]"
-                        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-primary"
-                        placeholder="Type the notes">
-                    </td>
-                    <td class="px-2 text-center">
-                      <button type="button"
-                        class="text-sm delete-row bg-danger bg-opacity-10 text-danger hover:underline underline-offset-2 px-4 py-2 rounded-lg">Remove</button>
-                    </td>
+          <select name="products[${rowCount}][product_name]"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-primary focus:border-primary block w-full p-2"
+            required>
+            <option selected>Select a product</option>
+          </select>
+        </td>
+        <td class="px-2 w-full">
+          <div class="flex items-center justify-center">
+            <button onclick="decreaseValue(event)"
+              class="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
+              type="button">
+              <span class="sr-only">Quantity button</span>
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+              </svg>
+            </button>
+            <input type="number" name="products[${rowCount}][amount]"
+              class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block px-2.5 py-1"
+              value="0" required />
+            <button onclick="increaseValue(event)"
+              class="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
+              type="button">
+              <span class="sr-only">Quantity button</span>
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+              </svg>
+            </button>
+          </div>
+        </td>
+        <td class="px-2">
+          <input type="text" name="products[${rowCount}][notes]"
+            class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-primary"
+            placeholder="Type ingredient name">
+        </td>
+        <td class="px-2 py-3">
+            <select id="countries" name="products[${rowCount}][metric]"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-primary focus:border-primaring-primary block w-full p-2">
+                <option selected>Select a metric</option>
+            </select>
+        </td>
+        <td class="px-2 text-center">
+          <button type="button" class="text-sm delete-row bg-danger bg-opacity-10 text-danger hover:underline underline-offset-2 px-4 py-2 rounded-lg">Remove</button>
+        </td>
                   </tr> --}}
               </tbody>
             </table>
@@ -113,7 +113,6 @@
               <span>Submit</span>
             </button>
           </div>
-
         </form>
       </div>
     </div>

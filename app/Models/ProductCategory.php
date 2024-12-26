8ed586b0f229fed6984ce0aad30cb5258c35dbd6
fernaldy <fernaldy@ad-ins.com>
2024-12-26 18:29:20 +0700

    Add Relationship

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductCategory extends Model
 {
     use HasFactory;
+
+    public function Branch() : BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function Products() : HasMany
+    {
+        return $this->hasMany(Product::class, 'product_category_id');
+    }
 
     public static function GetProductCategoryIdByProductCategoryCode(string $productCategoryCode){
 

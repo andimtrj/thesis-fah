7ce8ef89c468236820d2694f1577ca99a4ab10e0
fernaldy <fernaldy@ad-ins.com>
2024-12-26 18:30:33 +0700

    Add SoftDeletes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,10 +9,11 @@ use App\Models\ProductIngredientH;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'product_code' ,

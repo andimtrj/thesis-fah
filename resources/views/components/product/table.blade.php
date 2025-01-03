3630dfd0f71be9485c6ba49ec0432162ed5ec629
fernaldy <fernaldy@ad-ins.com>
2025-01-03 20:52:43 +0700

    Add Delete function untuk Product Page

--- resources/views/components/product/table.blade.php
+++ resources/views/components/product/table.blade.php
@@ -1,3 +1,4 @@
+<meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- Table --}}
     <div class="relative overflow-x-auto bg-white rounded-xl shadow-container">
       <table class="min-w-full leading-normal">
@@ -40,7 +41,14 @@
                             d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                         </svg>
                     </a>
-                    {{-- <x-product.delete-product /> --}}
+                    <a data-modal-target="deleteModal" data-modal-toggle="deleteModal" class="border-2 w-fit p-1 rounded-lg cursor-pointer"
+                        href="" onclick="confirmation(event, {{ $product->id }})">
+                        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                        </svg>
+                    </a>
                     </td>
                 </tr>
             @endforeach
@@ -52,5 +60,57 @@
             @endif
             </tr>
         </tbody>
-          </table>
-    </div>
+    </table>
+</div>
+
+<script>
+
+    function confirmation(event, id) {
+        event.preventDefault();
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Make a POST request to your delete route
+                fetch(`/delete-product/${id}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                    },
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to delete the item');
+                    }
+                })
+                .then(data => {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    }).then(() => {
+                        // Optionally, reload the page or remove the deleted item from the DOM
+                        location.reload(); // Or manipulate DOM to remove the deleted row
+                    });
+                })
+                .catch(error => {
+                    Swal.fire({
+                        title: "Error!",
+                        text: "There was an issue deleting the file.",
+                        icon: "error"
+                    });
+                    console.error('Error:', error);
+                });
+            }
+        });
+    }
+</script>
+
